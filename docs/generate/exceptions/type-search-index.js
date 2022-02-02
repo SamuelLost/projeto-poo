4ed97bf2ba0b0b25fe7fd36eb32f0f9f604b1564
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-02-02 19:42:43 -0300

    Gerando novo javadoc

--- docs/generate/exceptions/type-search-index.js
+++ docs/generate/exceptions/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"exceptions","l":"ClienteServicesException"},{"p":"exceptions","l":"CPFInvalidException"},{"p":"exceptions","l":"FilmeServicesException"},{"p":"exceptions","l":"SalaServicesException"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"exceptions","l":"ClienteServicesException"},{"p":"exceptions","l":"FilmeServicesException"},{"p":"exceptions","l":"SalaServicesException"}]
