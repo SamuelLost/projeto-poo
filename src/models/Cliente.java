b5eb3698935fe91f53b34131baf419285eef527c
SamuelLost <samuelhenriq12@gmail.com>
2022-02-02 21:07:24 -0300

    Ajeitando a classe abstrata Pessoa

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,10 +1,6 @@
 package models;
 
-public class Cliente extends Pessoa implements Comparable<Cliente> {
-    private String nome;
-    private String cpf;
-    private short idade;
-
+public class Cliente extends Pessoa {
     /**
      * Construtor para a classe
      * @param nome - nome do cliente
@@ -12,42 +8,7 @@ public class Cliente extends Pessoa implements Comparable<Cliente> {
      * @param idade - idade do cliente
      */
     public Cliente(String nome, String cpf, short idade) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idade = idade;
+        super(nome, cpf, idade);
     }
-
-    // /**
-    //  * GETTERS e SETTERS
-    //  */
-
-    /**
-     * Imprimindo informações do cliente
-     */
-    @Override
-    public String toString() {
-        String out = "==========================\n";
-        out += "Nome: " + this.nome + "\n";
-        out += "Idade: " + this.idade + "\n";
-        out += "CPF: " + this.cpf;
-        return out;
-    }
-
-    /**
-     * Override do método compareTo() da interface Comparable.
-     * Esse método é sobrescrito para que possa ser possível a ordenação
-     * de objetos Clientes. A comparação e ordenação é feita a partir 
-     * de seus nomes usando o próprio compareTo() da classe String.
-     *  
-     * @param o - objeto cliente
-     * @return um <code>integer<code> representando a comparação. 
-     * 0 - se for igual;
-     * menor que 0 se o nome do objeto for menor lexicograficamente que a passada por argumento;
-     * maior que 0 se o nome do objeto for maior lexicograficamente que a passada por argumento.
-     */
-    @Override
-    public int compareTo(Cliente o) {
-        return this.nome.compareTo(o.getNome());
-    }
-
+    
 }
--- src/models/Pessoa.java
+++ src/models/Pessoa.java
@@ -1,11 +1,16 @@
 package models;
 
 //Classe abstrata para uma pessoa
-public abstract class Pessoa {
+public abstract class Pessoa implements Comparable<Pessoa> {
     private String nome; // nome da pessoa
     private String cpf; // cpf da pessoa
     private short idade; // idade da pessoa
 
+    public Pessoa(String nome, String cpf, short idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
     /**
      * GETTERS e SETTERS
      */
@@ -28,6 +33,32 @@ public abstract class Pessoa {
         this.idade = idade;
     }
 
-    //Método abstrato que vai ser sobrescrito
-    public abstract String toString();
+    /**
+     * Imprimindo informações do cliente
+     */
+    @Override
+    public String toString() {
+        String out = "==========================\n";
+        out += "Nome: " + this.nome + "\n";
+        out += "Idade: " + this.idade + "\n";
+        out += "CPF: " + this.cpf;
+        return out;
+    }
+
+    /**
+     * Override do método compareTo() da interface Comparable.
+     * Esse método é sobrescrito para que possa ser possível a ordenação
+     * de objetos Pessoas. A comparação e ordenação é feita a partir 
+     * de seus nomes usando o compareToIgnoreCase() da classe String.
+     *  
+     * @param o - objeto pessoa
+     * @return um <code>integer<code> representando a comparação. 
+     * 0 - se for igual;
+     * menor que 0 se o nome do objeto for menor lexicograficamente que a passada por argumento;
+     * maior que 0 se o nome do objeto for maior lexicograficamente que a passada por argumento.
+     */
+    @Override
+    public int compareTo(Pessoa o) {
+        return this.getNome().compareToIgnoreCase(o.getNome());
+    }
 }
